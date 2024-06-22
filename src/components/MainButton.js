2aa7011eee0104bf33ebcc918d5fa0631aff9c30
Ahmed Adel <ahmedadel0239@gmail.com>
2024-06-22 05:52:16 +0300

    customize button type

--- src/components/MainButton.js
+++ src/components/MainButton.js
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 
-function MainButton({ btnIcon, btnTitle, clickHandler }) {
+function MainButton({ btnIcon, btnTitle, clickHandler, type }) {
   const { theme } = useSelector((state) => state.theme);
 
   return (
     <button
-      type="button"
+      type={type || "button"}
       className={`btn ${
         theme === "dark" ? "btn-light" : "btn-dark"
       } d-flex align-items-center justify-content-center fs-small fw-medium py-2 px-3 gap-1`}
