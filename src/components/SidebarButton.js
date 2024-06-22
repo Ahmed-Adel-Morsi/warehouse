9ce7b2894c6f04be12188639249dce231c99fa78
Ahmed Adel <ahmedadel0239@gmail.com>
2024-06-22 05:53:25 +0300

    on click handler added

--- src/components/SidebarButton.js
+++ src/components/SidebarButton.js
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
 
-function SidebarButton({ NavTo, icon, title }) {
+function SidebarButton({ NavTo, icon, title, onClick }) {
   return (
     <NavLink
       to={NavTo}
       className="list-group-item mb-1 list-group-item-action d-flex align-items-center gap-2 fw-semibold p-2 rounded border-0 btn-hov text-theme-color"
+      onClick={onClick}
     >
       {icon}
       {title}
