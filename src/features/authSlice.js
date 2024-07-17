553e7779e571e23904d1f8623dbae3fde1008df1
Ahmed Adel <ahmedadel0239@gmail.com>
2024-07-17 07:16:21 +0300

    auth method changed

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,12 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import apiCall from "../utils/apiCall";
 
-// Thunk for login
 export const loginUser = createAsyncThunk(
   "auth/loginUser",
   async (userCredentials, { rejectWithValue }) => {
-    const data = await apiCall(
-      `${process.env.REACT_APP_API_BASE_URL}/login`,
+    return await apiCall(
+      `/auth/login`,
       {
         method: "POST",
         headers: {
@@ -16,17 +15,14 @@ export const loginUser = createAsyncThunk(
       },
       rejectWithValue
     );
-    localStorage.setItem("token", data.token);
-    return data; // Return the data object (user and token)
   }
 );
 
-// Thunk for registration
 export const registerUser = createAsyncThunk(
   "auth/registerUser",
   async (userDetails, { rejectWithValue }) => {
-    const data = await apiCall(
-      `${process.env.REACT_APP_API_BASE_URL}/register`,
+    return await apiCall(
+      `/auth/register`,
       {
         method: "POST",
         headers: {
@@ -36,36 +32,17 @@ export const registerUser = createAsyncThunk(
       },
       rejectWithValue
     );
-    localStorage.setItem("token", data.token);
-    return data; // Note: Return the entire data object (user and token)
   }
 );
-
-// Thunk for token validation
-export const validateToken = createAsyncThunk(
-  "auth/validateToken",
-  async (token, { rejectWithValue }) => {
-    const data = await apiCall(
-      `${process.env.REACT_APP_API_BASE_URL}/validate-token`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ token }),
-      },
-      rejectWithValue
-    );
-    return data; // Return the data object (decoded token data)
-  }
-);
-
-// Initial token from localStorage
-const token = localStorage.getItem("token");
 
 const authSlice = createSlice({
-  initialState: { user: null, token, isLoading: false, error: null },
   name: "auth",
+  initialState: {
+    user: null,
+    token: localStorage.getItem("token"),
+    isLoading: false,
+    error: null,
+  },
   reducers: {
     logout: (state) => {
       localStorage.removeItem("token");
@@ -83,9 +60,11 @@ const authSlice = createSlice({
         state.error = null;
       })
       .addCase(loginUser.fulfilled, (state, action) => {
+        const { user, token } = action.payload;
         state.isLoading = false;
-        state.user = action.payload.user;
-        state.token = action.payload.token;
+        state.user = user;
+        state.token = token;
+        localStorage.setItem("token", token);
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
@@ -97,30 +76,15 @@ const authSlice = createSlice({
         state.error = null;
       })
       .addCase(registerUser.fulfilled, (state, action) => {
+        const { user, token } = action.payload;
         state.isLoading = false;
-        state.user = action.payload.user;
-        state.token = action.payload.token;
+        state.user = user;
+        state.token = token;
+        localStorage.setItem("token", token);
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
-      })
-      // Token validation actions
-      .addCase(validateToken.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(validateToken.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.user = action.payload.decoded;
-        state.token = token;
-      })
-      .addCase(validateToken.rejected, (state, action) => {
-        state.isLoading = false;
-        state.user = null;
-        state.token = null;
-        state.error = action.payload;
-        localStorage.removeItem("token");
       });
   },
 });
