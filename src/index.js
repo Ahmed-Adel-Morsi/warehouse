6d07c65c7cb7209309aae068186092c05535f227
Ahmed Adel <ahmedadel0239@gmail.com>
2026-04-03 22:55:05 +0200

    refactor: Remove unused testing files and reportWebVitals function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
@@ -20,5 +19,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
